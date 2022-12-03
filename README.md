be7c03e42dd9a962bfdcdd0a1ff9a1cc187be911
Rok <22999041+roknr@users.noreply.github.com>
2022-12-04 00:37:07 +0100

    Solve day 3 challenge

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 This repository contains solutions to advent of code 2022 challenges.
 
 ## Goal
-The goal is to solve all of this year's challenges, probably using mainly C# and maybe trying out F#.
+The goal is to solve all of this year's challenges, probably using mainly C# and maybe trying out F# and
+to have fun with solving the challenges by over-engineering some and completely one-lining the others (or trying to), depending on the mood.
---
+++ Source/AdventOfCode2022.Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿var result1 = File.ReadLines("./input.txt")
+    .Where(line => !string.IsNullOrWhiteSpace(line))
+    .Select(line => line[..(line.Length / 2)]
+        .Intersect(line[(line.Length / 2)..])
+        .Single())
+    .Select(c => c >= 'a'
+        ? c - ('a' - 1)
+        : c - ('A' - 1) + 'z' - 'a' + 1)
+    .Sum();
+
+Console.WriteLine(result1);
+
+
+var result2 = File.ReadLines("./input.txt")
+    .Where(line => !string.IsNullOrWhiteSpace(line))
+    .Chunk(3)
+    .Select(group => group[0]
+        .Intersect(group[1])
+        .Intersect(group[2])
+        .Single())
+    .Select(c => c >= 'a'
+        ? c - ('a' - 1)
+        : c - ('A' - 1) + 'z' - 'a' + 1)
+    .Sum();
+
+Console.WriteLine(result2);
