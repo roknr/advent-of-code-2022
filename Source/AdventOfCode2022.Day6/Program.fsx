fcd24b8cb81847a67be0ee895fcd41f71accb2be
Rok <22999041+roknr@users.noreply.github.com>
2022-12-07 22:36:56 +0100

    Solve day 6 challenge

---
+++ Source/AdventOfCode2022.Day6/Program.fsx
@@ -0,0 +1,11 @@
+﻿open System.IO
+
+let input = File.ReadAllText("./input.txt")
+
+let findMarker (datastreamBuffer: string) (markerLength: int) =
+    seq { (markerLength - 1) .. (datastreamBuffer.Length - 1) }
+    |> Seq.find (fun index -> Set(datastreamBuffer[(index - (markerLength - 1)) .. index]).Count = markerLength)
+    |> (fun index -> index + 1)
+
+printfn "%d" (findMarker input 4)
+printfn "%d" (findMarker input 14)
